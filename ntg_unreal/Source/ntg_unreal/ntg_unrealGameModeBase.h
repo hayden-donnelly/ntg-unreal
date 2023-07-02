c1ca25dee517f74fa4eb2259082ee57270459697
hayden-donnelly <donnellyhd@outlook.com>
2023-07-02 15:29:50 -0400

    Created ue project.

---
+++ ntg_unreal/Source/ntg_unreal/ntg_unrealGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ntg_unrealGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NTG_UNREAL_API Antg_unrealGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
